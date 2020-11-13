77755de7f441d91507987e0a10f99bfb2abf086c
Pedro Fernández Soler <56583826+peferso@users.noreply.github.com>
2020-11-13 15:48:11 +0100

    Update ec2-database-instance.sh

--- user-data/ec2-database-instance.sh
+++ user-data/ec2-database-instance.sh
@@ -29,7 +29,33 @@ echo " Step 1: mySQL installation begins        "
 echo "                                          "
 sudo yum update –y
 
-
+# Commands to print the Jenkins admin password after bash login
+# as well as other useful info
+PUBLIC_IP=$(curl -s ifconfig.co)
+echo 'PUBLIC_IP='$PUBLIC_IP >> /etc/environment
+customfile=/etc/profile.d/custom.sh
+sudo touch $customfile
+sudo echo 'echo "   "' >> $customfile
+sudo echo 'echo "   "' >> $customfile
+sudo echo 'echo "   "' >> $customfile
+sudo echo 'echo " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ "' >> $customfile
+sudo echo 'echo " ================================== "' >> $customfile
+sudo echo 'echo " ################################## "' >> $customfile
+sudo echo 'echo " ================================== "' >> $customfile
+sudo echo 'echo " "" Welcome ${USER}" to the mySQL-database instance' >> $customfile
+sudo echo 'echo "   "' >> $customfile
+sudo echo 'echo "   "' >> $customfile
+sudo echo 'echo " ""The instance public IP address is:"' >> $customfile
+sudo echo 'echo "   "' >> $customfile
+sudo echo 'echo "   "$PUBLIC_IP' >> $customfile
+sudo echo 'echo "   "' >> $customfile
+sudo echo 'echo " ================================== "' >> $customfile
+sudo echo 'echo " ################################## "' >> $customfile
+sudo echo 'echo " ================================== "' >> $customfile
+sudo echo 'echo " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ "' >> $customfile
+sudo echo 'echo "   "' >> $customfile
+sudo echo 'echo "   "' >> $customfile
+sudo echo 'echo "   "' >> $customfile
 echo "                                          "
 echo " Current Date-time stamp (YYYYMMDD HH:MM:SS:mS)"
 echo "$(date +'%Y%m%d %H:%M:%S:%3N')"
