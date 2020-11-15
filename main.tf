017eaee8de037b2708d03a0d30915985c5d87274
Pedro Fernández Soler <56583826+peferso@users.noreply.github.com>
2020-11-15 17:50:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "vanilla-ec2" {
   ami                         = "ami-0de12f76efe134f2f"
   instance_type               = "t2.micro"
   subnet_id                   = var.my_subnet_1
-  vpc_security_group_ids      = var.security_group_SSH_laptop
+  vpc_security_group_ids      = [aws_security_group.instance_ec2.id]
   associate_public_ip_address = true
   user_data                   = file("user-data/ec2-vanilla-instance.sh")
   key_name                    = "key-pair-Linux-AMI-root"
@@ -43,7 +43,7 @@ resource "aws_instance" "database-ec2" {
   ami                         = "ami-0de12f76efe134f2f"
   instance_type               = "t2.micro"
   subnet_id                   = var.my_subnet_1
-  vpc_security_group_ids      = var.security_group_SSH_laptop
+  vpc_security_group_ids      = [aws_security_group.database_ec2.id]
   associate_public_ip_address = true
   user_data                   = file("user-data/ec2-database-instance.sh")
   key_name                    = "key-pair-Linux-AMI-root"
