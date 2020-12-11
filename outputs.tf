3c600e1744b39b35dc381ee8fcb6d144828a49a2
Pedro Fernández Soler <56583826+peferso@users.noreply.github.com>
2020-12-11 15:56:50 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -25,6 +25,34 @@ output "EC2Database_insState" {
   value       = module.EC2Database.insState
 }
 
+# + Outputs from EC2Vanilla Module
+#   - insID (string)
+#   - insPublicIP (string)
+#   - insPrivateIP (string)
+#   - insPublicDNS (string)
+#   - insState (string)
+
+output "EC2Vanilla_insID" {
+  description = "ID of the client EC2 instance"
+  value       = module.EC2Vanilla.insID
+}
+output "EC2Vanilla_insPublicIP" {
+  description = "Public IP of the client EC2 instance"
+  value       = module.EC2Vanilla.insPublicIP
+}
+output "EC2Vanilla_insPrivateIP" {
+  description = "Private IP of the client EC2 instance"
+  value       = module.EC2Vanilla.insPrivateIP
+}
+output "EC2Vanilla_insPublicDNS" {
+  description = "Public DNS of the client EC2 instance"
+  value       = module.EC2Vanilla.insPublicDNS
+}
+output "EC2Vanilla_insState" {
+  description = "State of the client EC2 instance"
+  value       = module.EC2Vanilla.insState
+}
+  
 # + Outputs from SecurityGroups Module
 #   - clientEC2SecGrID (string)
 #   - serverEC2SecGrID (string)
