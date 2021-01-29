045c310bb664699a3cc662aaa2fb90b8f93e80c8
peferso <p.fdez.s.90@gmail.com>
2021-01-29 11:56:51 +0000

    reorganization of scripts

---
+++ Utilities/bash-scripts/create-environment.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+ENVNAME=${1}
+TEMPLATESDIR="../environment-template/"
+TEMPLATE="environment"
+TEMPLATEFILE="${TEMPLATESDIR}/${TEMPLATE}.txt"
+TFFILE="${TEMPLATE}_${ENVNAME}.tf"
+MAINDIR=".."
+
+HELPMSSG="
+# ========================================================
+# + Run it as follows ->
+# >
+# >   ./create-environment.sh \"Name of new environment\"
+# >  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ========================================================
+"
+
+ERRMSSGA="
+# ========================================================
+# + Error message:
+# >
+# > The environment \"${ENVNAME}\" is running
+# > The file \"${MAINDIR}/${TFFILE}\" already exists
+# ========================================================
+"
+  
+if [ -z ${1} ]
+then
+  echo "$HELPMSSG"
+  exit
+fi
+
+exists=$( 2>/dev/null ls ${MAINDIR}/${TFFILE} )
+if [ -z ${exists} ]
+then
+ echo "File template ${MAINDIR}/${TFFILE} does not exists"
+else
+ echo "${ERRMSSGA}"
+ echo " "
+ echo "Current *tf files:"
+ echo " "
+ ls -lrth .. | grep -i tf
+ exit
+fi
+
+echo "... Creating template"
+cp ${TEMPLATEFILE} ${MAINDIR}/${TFFILE}
+sed -i -e "s/ENVNAME/${ENVNAME}/g" ${MAINDIR}/${TFFILE}
+
+cd ..
+echo "... updating modules"
+terraform init
+
+echo "... applying changes"
+terraform apply -auto-approve
+
+echo ""
+echo "Infrastructure created."
+echo ""
+cd -
---
+++ Utilities/bash-scripts/destroy-environment.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+ENVNAME=${1}
+TEMPLATESDIR="../environment-template/"
+TEMPLATE="environment"
+TEMPLATEFILE="${TEMPLATESDIR}/${TEMPLATE}.txt"
+TFFILE="${TEMPLATE}_${ENVNAME}.tf"
+MAINDIR=".."
+
+HELPMSSG="
+# ========================================================
+# + Run it as follows ->
+# >
+# >   ./destroy-environment.sh \"Name of existing environment\"
+# >  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+"
+
+ERRMSSGA="
+# ========================================================
+# + Error message: 
+# >
+# > The environment \"${ENVNAME}\" is not running
+# > The file \"${MAINDIR}/${TFFILE}\" does not exists 
+# ========================================================
+"
+
+if [ -z ${1} ]
+then
+  echo "$HELPMSSG"
+  exit
+fi
+
+exists=$( 2>/dev/null ls ${MAINDIR}/${TFFILE} )
+if [ -z ${exists} ]
+then
+ echo "${ERRMSSGA}"
+ echo "Current *tf files:"
+ echo " "
+ ls -lrth .. | grep -i tf
+ exit
+fi
+
+rm -f ${MAINDIR}/${TFFILE}
+
+cd ..
+terraform apply -auto-approve
+cd -
