3b4ff89589b4f598a8fa04b604dd7778766dc695
EC2 Default User <ec2-user@ip-10-10-1-104.eu-west-3.compute.internal>
2021-02-01 17:37:39 +0000

    list of private ips recovered from tf refresh and included in array

--- Utilities/bash-scripts/update-ansible-inventory.sh
+++ Utilities/bash-scripts/update-ansible-inventory.sh
@@ -10,22 +10,51 @@ INITMSSG="
 # > instances and private ip's and consistently updates
 # > the list of Ansible managed hosts:
 # >  /etc/ansible/hosts
+# > 
+# + To run it just call:
+# >    ./update-ansible-inventory.sh -a
+# >    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # ========================================================
-To proceed with the execution type y[yes]:
-...
 "
 
 
 confirm() {
-  echo ${INITMSSG}
-  
+  if [ "$1" != "-a" ]
+  then
+    echo "${INITMSSG}"
+    exit
+  fi
 }
 
 find_ips () {
-	cd ${MAINDIR}
-	outputList=$(terraform output)
-        cd -
-	echo ${outputList}
+  cd ${MAINDIR}
+
+  outputList="$(terraform output | grep -i privateIp)"
+
+  delimiter="="
+  modNames=();
+  insPrvIP=();
+  while IFS= read -r line
+  do
+    partLeftt=${line%%"$delimiter"*}
+    partRight=${line#*"$delimiter"}
+    partRight=$( echo  "${partRight//\"/}"  )
+    modNames+=( ${partLeftt} );
+    insPrvIP+=( ${partRight} );
+    echo "Line:"
+    echo "$line"
+    echo "${partLeftt}"
+    echo "${partRight}"
+    echo " "
+
+  done <<< "${outputList}"
+  
+  echo " ${modNames[@]} "
+  echo " ${insPrvIP[@]} "
+
+  cd -
 }
+
+confirm $1
 
 find_ips
