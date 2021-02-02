85636e3a3dc14017cc46355797a53273391a823a
peferso <p.fdez.s.90@gmail.com>
2021-02-02 15:28:20 +0000

    playbook corrected error of pymysql installation due to old pip version

--- Utilities/bash-scripts/post-creation-tasks.sh
+++ Utilities/bash-scripts/post-creation-tasks.sh
@@ -1,49 +1,10 @@
 #!/bin/bash
 
-func_splitter() {
-  str=$1
-  delimiter=$2
-  s=$str$delimiter
-  array=();
-  while [[ $s ]]; do
-      array+=( "${s%%"$delimiter"*}" );
-      s=${s#*"$delimiter"};
-  done;  
+PLAYBOOKSDIR="../ansible-playbooks"
+DBSETUP="db-server-setup.yml"
+
+run_ansible_playbook() {
+  ansible ${PLAYBOOKSDIR}/${1}
 }
 
-pattern='{ "module":'
-tfstatestring=$(cat ./terraform.tfstate)
-tfstatestring=$(echo $tfstatestring)
-func_splitter "$tfstatestring" "$pattern"
-
-array_full=( "${array[@]}" ) # Store the initial array
-
-# Iterate over array values locating module names and private ips
-echo " "
-echo " "
-echo "----------------------------------------------------  "
-echo " "
-echo " "
-modNames=();
-for i in "${array_full[@]}"
-do
-  func_splitter "$i" ", "
-  modNames+=${array[0]}
-  for j in "${i[@]}"
-  do
-    func_splitter "$j" ", "
-  done
-done
-echo " "
-echo " "
-echo "----------------------------------------------------  "
-echo " "
-echo " "
-
-
-
-
-echo " "
-echo " "
-echo " -------------- "
-echo "++ 0: ${modNames}"
+run_ansible_playbook "${DBSETUP}"
