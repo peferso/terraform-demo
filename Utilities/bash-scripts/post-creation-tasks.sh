2569f7edb61557c28b69d474a63c019421bca52a
peferso <p.fdez.s.90@gmail.com>
2021-02-04 11:00:14 +0000

    changes to avoid ssh (yes) confirmation first time

--- Utilities/bash-scripts/post-creation-tasks.sh
+++ Utilities/bash-scripts/post-creation-tasks.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 
-PLAYBOOKSDIR="../ansible-playbooks"
+
+PLAYBOOKSDIR="/home/ec2-user/tf_templates/Utilities/ansible-playbooks"
 DBSETUP="db-server-setup.yml"
+ANSCONFIGFILE="ansible.cfg"
+
+create_environment_variable_ansiblecfg_dir() {
+  MSSGENV="
+  # ========================================================
+  # + ... Using .cfg file:
+  # > ${PLAYBOOKSDIR}/${1}
+  # ========================================================
+  "
+  echo "${MSSGENV}"
+  export ANSIBLE_CONFIG=${PLAYBOOKSDIR}/${1}
+}
+
+delete_environment_variable_ansiblecfg_dir() {
+  unset ANSIBLE_CONFIG
+}
 
 run_ansible_playbook() {
+  MSSGPLBK="
+  # ========================================================
+  # + ... Running the playbook: 
+  # > ${PLAYBOOKSDIR}/${1}
+  # ========================================================
+  "
+  echo "${MSSGPLBK}"
   ansible-playbook ${PLAYBOOKSDIR}/${1}
 }
 
+# Main
+
+create_environment_variable_ansiblecfg_dir "${ANSCONFIGFILE}"
+
 run_ansible_playbook "${DBSETUP}"
+
+delete_environment_variable_ansiblecfg_dir 
